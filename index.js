cedebde6b861812a3395c5fe24090b340295cbd0
developerX <x@developerx.io>
2019-04-23 14:23:21 -0700

    added in a bit of notes

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 require('dotenv').config();
+// Express Middleware
 const express = require('express');
+// Body Parser Middle ware
 const bodyParser = require('body-parser');
+// Stripe Library
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+// Enable Cors
 const cors = require('cors');
+//Set up application
 const app = express();
 
-
+//Use Cors
 app.use(cors())
+//Use Body Parser
 app.use(bodyParser.json());
 
+// Create Dummy Route to test if working
 app.get('/', (request, response) => {
   response.send("I am have successfully started a backend server");
 });
 
+// Create Payment Route
 app.post('/payment', (request, response) => {
   // Stripe Charges
   stripe.charges.create({
@@ -36,6 +44,7 @@ app.post('/payment', (request, response) => {
     })
 })
 
+// Activate Server to start listening to defined port
 app.listen(process.env.PORT, () => {
   console.log(`I am live on port ${process.env.PORT}`);
 });
